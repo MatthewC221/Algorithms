e658725e7d4b01fee5eadf75d836b2df2564e0b1
MatthewC221 <matthewchen42@gmail.com>
2017-03-19 03:53:30 +1100

    Knuth-Morris-Pratt
    O(n) consistently. I am still a bit confused why a hash is better?

---
+++ substring.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+# Implementation of Knuth-Morris-Pratt, doesn't require hash implementation, seems much more elegant?
+
+# First argument is the substring
+
+import sys
+
+if (len(sys.argv) == 2):
+    substring = sys.argv[1]
+else:
+    print "./substring.py <substring>"
+    sys.exit()
+
+def test_string(location, string_text):
+
+    for i in range(0, len(substring)):
+        if (substring[i] != string_text[location+i]):
+            return i
+
+    print string_text[location:location+i+1]
+    return -1
+
+file = open("string_substring.txt", "r")
+
+string_text = file.read()
+
+in_substring = 0
+i = 0
+
+while (i < len(string_text)):
+    print string_text[i],
+    if (string_text[i] == substring[0]):
+        temp = test_string(i, string_text)
+        if (temp != -1):
+            i = i + temp - 1 
+    i += 1
+    
+    
