8dbeb7114a0be5d45821dd46f65202f086ab1f70
MatthewC221 <matthewchen42@gmail.com>
2017-06-21 22:53:54 +1000

    Completed!

--- frog_jump.py
+++ frog_jump.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/frog-jump/#/description
-# TLE on leetcode, I am working on timing issues, it's currently a DFS, BFS was too slow for bigger inputs. 
-# I'm quite sure it works.
+# My first hard problem solved on leetcode! The big challenge here is the time limit. By rearranging it
+# and doing the biggest jumps first I can solve it. Beats 76.77% (not that bad :))
+# Uses DFS
 
 class Solution(object):
     def canCross(self, stones):
@@ -13,12 +14,10 @@ class Solution(object):
         dp.append((0, 1))
         current_pos = 0
         
-        # If not possible
         for i in range(1, len(stones)):
             if (stones[i] - stones[i - 1] > i):
                 return False
-        
-        # While the list is not empty
+                
         while (dp and current_pos < size):
             first = dp.pop(0)
             current_pos = first[0]
@@ -26,24 +25,25 @@ class Solution(object):
             
             attempt_jump_position = current_pos + jump_size
             
-            # If last stone
             if (attempt_jump_position == stones[len(stones) - 1]):
                 return True
 
             if (attempt_jump_position in stones):    # If there is a stone
-                # The three types of jumps!
-                standard_jump = ((attempt_jump_position, jump_size))
-                if (standard_jump not in dp):
-                    dp.insert(0, standard_jump)
                 
-                larger_jump = ((attempt_jump_position, jump_size + 1))
-                if (larger_jump not in dp):
-                    dp.insert(0, larger_jump)
-                    
+                # Smallest stone is inserted first (therefore will be at index 2 when the large ones come in)
                 if (jump_size > 1):
                     smaller_jump = ((attempt_jump_position, jump_size - 1)) 
                     if (smaller_jump not in dp):
                         dp.insert(0, smaller_jump)
+
+                standard_jump = ((attempt_jump_position, jump_size))
+                if (standard_jump not in dp):
+                    dp.insert(0, standard_jump)
+                
+                # Do largest jumps first
+                larger_jump = ((attempt_jump_position, jump_size + 1))
+                if (larger_jump not in dp):
+                    dp.insert(0, larger_jump)
         
         
         return False
