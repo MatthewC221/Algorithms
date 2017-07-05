acb933f9e75a461faa4f6b75304426d2e7734d73
MatthewC221 <matthewchen42@gmail.com>
2017-07-05 18:06:34 +1000

    a leetcode question

---
+++ max_product_3.py
@@ -0,0 +1,34 @@
+# Leetcode easy: https://leetcode.com/problems/maximum-product-of-three-numbers/#/description
+# Slightly tricky unless you realise line 10.
+
+class Solution(object):
+    def maximumProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Max = max of top three numbers or top number and smallest negative numbers
+        # O(N)
+        
+        min_arr = [1001] * 2
+        max_arr = [-1001] * 3
+        
+        for i in range(len(nums)):
+            if (nums[i] < min_arr[0]):
+                min_arr[1] = min_arr[0]
+                min_arr[0] = nums[i]
+            elif (nums[i] < min_arr[1]):
+                min_arr[1] = nums[i]
+                
+            if (nums[i] > max_arr[0]):
+                max_arr[2] = max_arr[1]
+                max_arr[1] = max_arr[0]
+                max_arr[0] = nums[i]  
+            elif (nums[i] > max_arr[1]):
+                max_arr[2] = max_arr[1]
+                max_arr[1] = nums[i]
+            elif (nums[i] > max_arr[2]):
+                max_arr[2] = nums[i]
+        
+        return (max(max_arr[0] * max_arr[1] * max_arr[2], 
+                    max_arr[0] * min_arr[1] * min_arr[0]))
