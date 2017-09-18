1969ad7e5e3290775787be207f458e030d1db4ec
Matthew Chen <matthewchen42@gmail.com>
2017-09-18 23:29:17 +1000

    update max_subarray.py

--- max_subarray.py
+++ max_subarray.py
@@ -1,6 +1,6 @@
 # Leetcode: https://leetcode.com/problems/maximum-subarray/description/
 # Pretty straight forward DP, not as challenging as the multiplication form
-# The first solution seems like it would be faster, but the second beats 60%, first beats 20%
+# The first solution seems like it would be faster, but the second beats 73%, first beats 20%
 
 class Solution(object):
     def maxSubArray(self, nums):
@@ -19,7 +19,7 @@ class Solution(object):
         
         """
         max_sub = nums[:]
-        for i in range(1, len(nums)):
+        for i in xrange(1, len(nums)):
             max_sub[i] = max(max_sub[i - 1] + nums[i], max_sub[i])
       
         return max(max_sub)
