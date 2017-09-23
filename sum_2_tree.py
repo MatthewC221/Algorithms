6902736d1f636bad0a51b2111d664de0ae88dd9a
Matthew Chen <matthewchen42@gmail.com>
2017-09-23 22:16:06 +1000

    update sum_2_tree.py

--- sum_2_tree.py
+++ sum_2_tree.py
@@ -1,5 +1,6 @@
 # Leetcode: https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
 # Basically can use a hash/set to track numbers, too slow if two-sum downwards for each node
+# Dict is faster, beats 90% with dict
 
 # Definition for a binary tree node.
 # class TreeNode(object):
@@ -22,13 +23,15 @@ class Solution(object):
         
         queue = [root]
         nums = set()
+        # nums = {}
         
         while (len(queue) > 0):
             tmp = queue.pop()
             if (k - tmp.val in nums):
                 return True
             
-            nums.add(tmp.val)    
+            nums.add(tmp.val)
+            # nums[tmp.val] = 1
             if (tmp.left):
                 queue.append(tmp.left)
             if (tmp.right):
