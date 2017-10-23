14bfc1bfb41bbf22b0cb236aea798dcf1c3c5db4
Matthew Chen <matthewchen42@gmail.com>
2017-10-23 15:05:45 +1100

    create search_insert.py

---
+++ search_insert.py
@@ -0,0 +1,33 @@
+# Leetcode: https://leetcode.com/problems/search-insert-position/description/
+# Binary search with twist, be wary of edge cases. In the binary search, where you would normally exit
+# if not found, such as line 26 return that index. (the higher one).
+# E.g: [1, 5, 7, 9], looking for 6. Low and high will be [5, 7] therefore return index of 7
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        # Do bin search, if it's not found. Use the last indexes
+        if not nums: return 0
+        
+        if (target < nums[0]): return 0
+        if (target > nums[-1]): return len(nums)
+        if (target == nums[0]): return 0
+    
+        low = 0
+        high = len(nums) - 1
+        
+        while (low < high):
+            mid = (low + high) / 2
+            print nums[mid]
+            if (nums[mid] == target): return mid
+            else:
+                if (low + 1 == high): return high
+                if (nums[mid] > target): high = mid
+                elif (nums[mid] < target): low = mid
+        
+        return -1
